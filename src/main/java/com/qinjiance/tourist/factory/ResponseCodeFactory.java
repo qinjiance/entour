e85036545eced32b53d16952759c7e9150b6da14
Jiance Qin <qinjiance@126.com>
2016-04-05 14:57:10 +0800

    update

--- src/main/java/com/qinjiance/tourist/factory/ResponseCodeFactory.java
+++ src/main/java/com/qinjiance/tourist/factory/ResponseCodeFactory.java
@@ -2,10 +2,6 @@ package com.qinjiance.tourist.factory;
 
 import module.laohu.commons.model.ResponseCode;
 
-/**
- * @author tianbenzhen@pwrd.com
- * @version 2014-7-2 下午12:04:04
- */
 public class ResponseCodeFactory {
 	
 	public static ResponseCode responseCode(){
